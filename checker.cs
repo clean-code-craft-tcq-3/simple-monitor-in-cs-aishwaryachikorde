f4406db5e785a77ddf420d2a7a1f0dd25233224d
Aishwarya Chikorde <41798723+aishwaryachikorde@users.noreply.github.com>
2022-05-19 16:04:40 +0530

    Add files via upload

---
+++ checker.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace checker
+{
+  public class BatteryManagement: IBatteryManagement
+  {
+    public bool IsBatteryOk(float temperature, float soc, float chargeRate)
+    {
+      return IsTemperatureInRange(temperature) && IsSocInRange(soc) && IsChargeRateInRange(chargeRate);
+    }
+
+    public bool IsTemperatureInRange(float temperature)
+    {
+      if (IsParameterUnderLimit(45,0,temperature) == false)
+      {
+        PrintBatteryStatus("Temperatue is out of range!");
+        return false;
+      }
+
+      return true;
+    }
+
+    public bool IsSocInRange(float stateOfCharge)
+    {
+      if (IsParameterUnderLimit(80, 20, stateOfCharge) == false)
+      {
+        PrintBatteryStatus("State of Charge is out of range!");
+        return false;
+      }
+
+      return true;
+    }
+    
+    private static void PrintBatteryStatus(string statusMessage)
+    {
+      Console.WriteLine(statusMessage);
+    }
+
+    private bool IsParameterUnderLimit(int maxValue, int minValue, float actualValue)
+    {
+      if (actualValue > maxValue || actualValue <  minValue)
+      {
+        return false;
+      }
+
+      return true;
+    }
+
+    public bool IsChargeRateInRange(float chargeRate)
+    {
+      if (chargeRate > 0.8f)
+      {
+        PrintBatteryStatus("Charge Rate is out of range!");
+        return false;
+      }
+
+      return true;
+    }
+  }
+}
